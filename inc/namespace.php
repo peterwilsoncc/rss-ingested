a417c8ab3df23899f69296a3d166d9912f5817f7
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2025-07-16 07:53:01 +1000

    Initial commit

---
+++ inc/namespace.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * WP Plugin Template
+ *
+ * @package           WpPluginTemplate
+ * @author            Peter Wilson
+ * @copyright         2023 Peter Wilson
+ * @license           MIT
+ */
+
+namespace PWCC\WpPluginTemplate;
+
+const PLUGIN_VERSION = '1.0.0';
+
+/**
+ * Bootstrap the plugin.
+ */
+function bootstrap() {
+}
---
+++ tests/class-test-sample.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Test Template
+ *
+ * @package PWCC\WpPluginTemplate\Tests
+ */
+
+namespace PWCC\WpPluginTemplate\Tests;
+
+use WP_UnitTestCase;
+
+/**
+ * Sample Tests
+ */
+class Test_Sample extends WP_UnitTestCase {
+	/**
+	 * Test passing test.
+	 */
+	public function test_passing_test() {
+		$this->assertTrue( true );
+	}
+}
