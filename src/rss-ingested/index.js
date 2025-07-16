47a87f0813b7922df11aaf46deecec669fd8bf83
Peter Wilson <git-commit-by@peterwilson.cc>
2025-07-16 12:09:08 +1000

    Block registration, JS.

--- src/rss-ingested/index.js
+++ src/rss-ingested/index.js
@@ -1,18 +1,7 @@
 /**
- * Registers a new block provided a unique name and an object defining its behavior.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ * WordPress dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * All files containing `style` keyword are bundled together. The code used
- * gets applied both to the front of your site and to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './style.scss';
 
 /**
  * Internal dependencies
@@ -20,14 +9,12 @@ import './style.scss';
 import Edit from './edit';
 import metadata from './block.json';
 
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
- */
-registerBlockType( metadata.name, {
-	/**
-	 * @see ./edit.js
-	 */
+const { name } = metadata;
+export { metadata, name };
+
+export const settings = {
+	example: {},
 	edit: Edit,
-} );
+};
+
+registerBlockType( { name, ...metadata }, settings );
